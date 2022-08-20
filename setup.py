518459c7f0e9195c95ab5cb0dc412bf9bcacdf5c
Vikram <vikramblaze1782000@gmail.com>
2022-08-20 19:51:53 +0530

    Initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Dashboard for Analyzing Numerai Data which is constantly changing',
+    author='Vikram',
+    license='MIT',
+)
